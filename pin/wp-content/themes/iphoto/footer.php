a4e57539332f0855c42177bbad3648a4c67eb371
FollenFang <Follen@Follen.us>
2014-11-30 21:08:56 +0800

    init

--- pin/wp-content/themes/iphoto/footer.php
+++
@@ -1,8 +0,0 @@
-	</div>
-<div id="footer">
-	<p>Copyright ? 2012 <a href="http://follen-zoe.us/pin" title="pin">Pin</a> All rights reserved</p><p>Powered by <a href="http://follen-zoe.us/wannatellu" title="Follen">Follen</a> </p>
-</div>
-<div id="scrollTop"><a href="#"><?php _e( 'Scroll to Top','iphoto');?></a></div>
-<?php wp_footer(); ?>
-</body>
-</html>
